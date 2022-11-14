4d81d55128e0006271da389455efa88939bbab6a
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-11-14 22:30:49 +0100

    add render after resize

--- src/sketch.js
+++ src/sketch.js
@@ -20,6 +20,7 @@ class Sketch {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.render();
     }
 
     createShapes() {
