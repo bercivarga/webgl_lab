225373c7702d2c230f997d6c1cdd0a6f5a47a8d6
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-12-07 22:25:15 +0100

    add hypnosis effect

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,8 @@
 precision mediump float;
 
 uniform sampler2D image;
-uniform vec2 mouse;
 uniform float pixelDensity;
+uniform float time;
 
 in vec2 vUv;
 
@@ -12,6 +12,12 @@ void main()	{
     vec4 imageBackground = texture2D(image, vUv);
     vec2 textUv = floor(vUv * pixelDensity) / pixelDensity;
     imageBackground = texture2D(image, textUv);
+
+    float distToCenter = length(vUv - 0.5);
+    float d = sin(distToCenter * 100.0 + time * 0.5) * 0.5 + 0.5;
+    d = smoothstep(0.0, 0.1, d);
+    imageBackground.rgb = vec3(0.0);
+    imageBackground.rgb = mix(imageBackground.rgb, vec3(1.0, 0.0, 0.0), d);
 
     gl_FragColor = imageBackground;
 }
