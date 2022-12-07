9e748b10160de13f34b62fa0ee98b2339cd7761d
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-12-07 22:29:36 +0100

    follow mouse

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,6 +2,7 @@ precision mediump float;
 
 uniform sampler2D image;
 uniform float pixelDensity;
+uniform vec2 mouse;
 uniform float time;
 
 in vec2 vUv;
@@ -13,9 +14,11 @@ void main()	{
     vec2 textUv = floor(vUv * pixelDensity) / pixelDensity;
     imageBackground = texture2D(image, textUv);
 
-    float distToCenter = length(vUv - 0.5);
+    vec2 mouseCoords = mouse + vec2(0.5, 0.5);
+
+    float distToCenter = length(vUv - mouseCoords);
     float d = sin(distToCenter * 100.0 + time * 0.5) * 0.5 + 0.5;
-    vec2 dir = normalize(vUv - 0.5);
+    vec2 dir = normalize(vUv - mouseCoords);
     vec2 rippleCoords = vUv + dir * d * 0.02;
 
     imageBackground = texture2D(image, rippleCoords);
